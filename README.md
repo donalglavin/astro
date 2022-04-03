64c2a77a3fe4eac141f9c5e158cf5c62273fce03
Donal Glavin <donal.l.glavin@gmail.com>
2022-04-03 20:11:08 +0930

    Removed org in favour for markdown

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+This is a file directory used to maintain my AstroVim user config init.lua.
+
--- init.lua
+++ init.lua
@@ -57,25 +57,42 @@ local config = {
           require("lsp_signature").setup()
         end,
       },
-      { "nvim-orgmode/orgmode",
-      ft = "org",
-      highlight = {
-        enable = true,
-        disable = {"org"},
-        additional_vim_regexp_highlighting = {"org"}
-      },
-      config = function()
-        --         require("orgmode").setup_ts_grammar()
-        require("orgmode").setup({
-          org_agenda_files = {"~/OneDrive - Evolution Mining Limited/Wiki/pages/**/*"},
-          org_default_notes = "~/OneDrive - Evolution Mining Limited/Wiki/pages/refile.org",
-        })
-      end,
-    }
-   },
-   -- All other entries override the setup() call for default plugins
-   treesitter = {
-     ensure_installed = { "lua", "python", "json", "powershell", "org" },
+
+      --       { 'nvim-treesitter/nvim-treesitter-textobjects' },
+      --       { "nvim-orgmode/orgmode",
+      --       config = function()
+        --         -- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        --         -- parser_config.org = {
+          --           -- install_info = {
+            --             -- url = 'https://github.com/milisims/tree-sitter-org',
+            --             -- revision = 'f110024d539e676f25b72b7c80b0fd43c34264ef',
+            --             -- files = {'src/parser.c', 'src/scanner.cc'},
+            --           -- },
+            --           -- filetype = 'org',
+            --         -- }
+            --
+            --         require('nvim-treesitter.configs').setup {
+              --           -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
+              --           highlight = {
+                --             enable = true,
+                --             disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
+                --             additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
+                --           },
+                --           ensure_installed = {'org'}, -- Or run :TSUpdate org
+                --         }
+                --
+                --         require('orgmode').setup({
+                  --           org_agenda_files = {"~/OneDrive - Evolution Mining Limited/Wiki/pages/**/*"},
+                  --           org_default_notes = "~/OneDrive - Evolution Mining Limited/Wiki/pages/refile.org",
+                  --         })
+                  --         require('orgmode').setup_ts_grammar()
+                  --
+                  --       end
+                  --     }
+  },
+  -- All other entries override the setup() call for default plugins
+  treesitter = {
+     ensure_installed = { "lua", "python", "json", "org" },
     },
     packer = {
       compile_path = vim.fn.stdpath "config" .. "/lua/packer_compiled.lua",
